86f16cc71dea1a774008f9a562f8d9a76fc6d32c
afernandez1303 <58273284+afernandez1303@users.noreply.github.com>
2020-06-16 09:21:38 +0200

    Create ie1nb.R

---
+++ INAR-package/some_inar_functions/ie1nb/ie1nb.R
@@ -0,0 +1,30 @@
+# R interface to the C code for evaluating the negative log-likelihood
+# of the INAR(1) model with generalized thinning operator and negative binomial innovations
+
+# param=(alpha, p, theta, gamma) 
+# xdata = data vector
+# iprint >=1 for printing of intermediate calculations
+# Returns negative log-likelihood based on forward probabilities
+
+setwd("/Users/Amanda/Dropbox/Thesis/Paper6-rPackage/ie1nb")
+ie1nb.nllk=function(param,xdata,iprint=1)
+{ 
+if(any(param<=0)) return(1.e10)
+if(param[1]>=1  ) return(1.e10)
+if(!is.loaded("ie1nb")) dyn.load("./ie1nb.so")
+n=length(xdata)
+xdata=as.integer(xdata)
+if(min(xdata)<0) stop("negative counts")
+icode=2
+nllk=0
+out=.C("ie1nb", as.integer(n), as.integer(xdata), 
+as.double(param), as.integer(iprint),
+as.integer(icode),nllk=as.double(nllk))
+out$nllk
+}
+
+#======================================================================
+
+xdata=c(1,3,0,4,6,7,8,3,3,4)
+params=c(0.5,0.6,3,0.1)
+res=ie1nb.nllk(param=params,xdata=xdata,iprint=2);res
